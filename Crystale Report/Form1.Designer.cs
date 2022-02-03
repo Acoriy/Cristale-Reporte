c74ea3b6969984f8ff48de232ade64cdcb6ee5a0
Acoriy <sofyane.acoriy@gmail.com>
2022-02-03 23:38:41 +0100

    l'affichage dans une cristale reporte viewer

---
+++ Crystale Report/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Crystale_Report
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textRechercher = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.btnRechercher = new System.Windows.Forms.Button();
+            this.btnImprimer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CIN";
+            // 
+            // textRechercher
+            // 
+            this.textRechercher.Location = new System.Drawing.Point(92, 25);
+            this.textRechercher.Name = "textRechercher";
+            this.textRechercher.Size = new System.Drawing.Size(232, 26);
+            this.textRechercher.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(443, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(758, 169);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 187);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1213, 434);
+            this.crystalReportViewer1.TabIndex = 3;
+            // 
+            // btnRechercher
+            // 
+            this.btnRechercher.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnRechercher.FlatAppearance.BorderSize = 0;
+            this.btnRechercher.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRechercher.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRechercher.Image = global::Crystale_Report.Properties.Resources.Rechercher;
+            this.btnRechercher.Location = new System.Drawing.Point(343, 12);
+            this.btnRechercher.Name = "btnRechercher";
+            this.btnRechercher.Size = new System.Drawing.Size(84, 54);
+            this.btnRechercher.TabIndex = 6;
+            this.btnRechercher.UseVisualStyleBackColor = false;
+            this.btnRechercher.Click += new System.EventHandler(this.btnRechercher_Click);
+            // 
+            // btnImprimer
+            // 
+            this.btnImprimer.FlatAppearance.BorderSize = 0;
+            this.btnImprimer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImprimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimer.Image = global::Crystale_Report.Properties.Resources.Imprimer;
+            this.btnImprimer.Location = new System.Drawing.Point(274, 95);
+            this.btnImprimer.Name = "btnImprimer";
+            this.btnImprimer.Size = new System.Drawing.Size(107, 72);
+            this.btnImprimer.TabIndex = 5;
+            this.btnImprimer.UseVisualStyleBackColor = true;
+            this.btnImprimer.Click += new System.EventHandler(this.btnImprimer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1213, 621);
+            this.Controls.Add(this.btnRechercher);
+            this.Controls.Add(this.btnImprimer);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textRechercher);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textRechercher;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Button btnImprimer;
+        private System.Windows.Forms.Button btnRechercher;
+    }
+}
+
---
+++ Crystale Report/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Crystale_Report
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void getStagaire()
+        {
+            DataSet1 ds = new DataSet1();
+            DataSet1TableAdapters.StagiaireTableAdapter stg = new DataSet1TableAdapters.StagiaireTableAdapter();
+            stg.Fill(ds.Stagiaire);
+            CrystaleAffiche CR = new CrystaleAffiche();
+            CR.SetDataSource(ds);
+            crystalReportViewer1.ReportSource = CR;
+        }
+        void getBayCin()
+        {
+            DataSet1 ds = new DataSet1();
+            DataSet1TableAdapters.Stagiaire1TableAdapter stag = new DataSet1TableAdapters.Stagiaire1TableAdapter();
+            stag.Fill(ds.Stagiaire1, textRechercher.Text);
+            CrystalRecherchCin rech = new CrystalRecherchCin();
+            rech.SetDataSource(ds);
+            crystalReportViewer1.ReportSource = rech;
+        }
+
+        private void btnRechercher_Click(object sender, EventArgs e)
+        {
+            if (textRechercher.Text != "")
+            {
+                DataSet1 ds = new DataSet1();
+                DataSet1TableAdapters.Stagiaire1TableAdapter stag = new DataSet1TableAdapters.Stagiaire1TableAdapter();
+                stag.Fill(ds.Stagiaire1, textRechercher.Text);
+                dataGridView1.DataSource = ds.Tables["Stagiaire1"];
+            }
+            else
+            {
+
+                DataSet1 ds = new DataSet1();
+                DataSet1TableAdapters.StagiaireTableAdapter stg = new DataSet1TableAdapters.StagiaireTableAdapter();
+                stg.Fill(ds.Stagiaire);
+                dataGridView1.DataSource = ds.Tables["Stagiaire"];
+            }
+        }
+
+        private void btnImprimer_Click(object sender, EventArgs e)
+        {
+            if (textRechercher.Text != "")
+            {
+                getBayCin();
+            }
+            else
+            {
+                getStagaire();
+            }
+        }
+    }
+}
